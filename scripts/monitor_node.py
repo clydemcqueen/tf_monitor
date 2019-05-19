035c1e1afc61fc687d7995e8e1bfabaebfd5f0da
Clyde McQueen <clyde@mcqueen.net>
2019-05-19 10:29:50 -0700

    Refactor, detect cycles

--- scripts/monitor_node.py
+++ scripts/monitor_node.py
@@ -2,7 +2,9 @@
 
 """ROS2 TF diagnostic tool"""
 
-from typing import List, Optional
+from typing import Dict, List, Tuple
+
+import transformations as xf
 
 import rclpy
 import rclpy.time
@@ -12,120 +14,99 @@ import geometry_msgs.msg
 
 import sim_node  # TODO remove in Dashing
 
-STALE_DURATION = rclpy.time.Duration(seconds=5)
+
+def q_to_rpy(q: geometry_msgs.msg.Quaternion) -> Tuple[float, float, float]:
+    matrix = xf.quaternion_matrix([q.w, q.x, q.y, q.z])  # Note order
+    return xf.euler_from_matrix(matrix, axes='sxyz')
 
 
 def tf_to_str(tf: geometry_msgs.msg.TransformStamped) -> str:
     t = tf.transform.translation
-    r = tf.transform.rotation
-    return f'({t.x:.2f}, {t.y:.2f}, {t.z:.2f}), ({r.x:.2f}, {r.y:.2f}, {r.z:.2f}, {r.w:.2f})'
+    r = q_to_rpy(tf.transform.rotation)
+    return f'<origin xyz="{t.x:.2f} {t.y:.2f} {t.z:.2f}" rpy="{r[0]:.2f}, {r[1]:.2f}, {r[2]:.2f}" />'
 
 
-class Frame:
-
-    def __init__(self, frame_id: str, tf: Optional[geometry_msgs.msg.TransformStamped] = None):
-        self._frame_id = frame_id
-        self._tf = tf  # None for root
-        self._children: List[Frame] = []
-
-    # Add or update a child frame
-    def add_or_update_child(self, tf: geometry_msgs.msg.TransformStamped) -> None:
-        for child in self._children:
-            if child._frame_id == tf.child_frame_id:
-                child._tf = tf
-                return
-        self._children.append(Frame(tf.child_frame_id, tf))
-
-    # Return True if this frame has child frames
-    def has_children(self) -> bool:
-        return len(self._children) > 0
-
-    # Find a frame in this tree, return None if not found
-    def find_frame_in_tree(self, frame_id: str):  # -> Frame
-        if frame_id == self._frame_id:
-            return self
-        for child in self._children:
-            result = child.find_frame_in_tree(frame_id)
-            if result is not None:
-                return result
-        return None
-
-    # Remove stale frames from a tree
-    def delete_stale_frames_from_tree(self, stamp: rclpy.time.Time) -> None:
-        # Recurse first
-        for child in self._children:
-            child.delete_stale_frames_from_tree(stamp)
-
-        # Check children
-        fresh: List[Frame] = []
-        for child in self._children:
-            if stamp - rclpy.time.Time.from_msg(child._tf.header.stamp) > STALE_DURATION:
-                print(f'{self._frame_id} => {child._frame_id} is stale, deleting')
-            else:
-                fresh.append(child)
-        self._children = fresh
-
-    # Print a tree
-    def print_tree(self, prefix: str = '') -> None:
-        for child in self._children:
-            print(f'{prefix}{self._frame_id} => {child._frame_id} {tf_to_str(child._tf)}')
-            child.print_tree(prefix + '...')
+# Print children of this parent, return count of transforms printed
+def print_children(parent: geometry_msgs.msg.TransformStamped,
+                   frames: Dict[str, geometry_msgs.msg.TransformStamped],
+                   prefix: str = '...') -> int:
+    num_printed = 0
+    for tf in frames.values():
+        if parent.child_frame_id == tf.header.frame_id:
+            print(f'{prefix}{tf.header.frame_id} => {tf.child_frame_id}: {tf_to_str(tf)}')
+            num_printed = num_printed + print_children(tf, frames, prefix + '...') + 1
+    return num_printed
 
 
 class MonitorNode(sim_node.SimNode):
 
     def __init__(self):
         super().__init__('monitor_node')
-        self._roots: List[Frame] = []
+
+        # tf_callback builds a dict of all transforms, keyed by (frame_id, child_frame_id)
+        self._transforms: Dict[Tuple[str, str], geometry_msgs.msg.TransformStamped] = {}
+
+        # Most nodes publish on /tf
         self.create_subscription(tf2_msgs.msg.TFMessage, '/tf', self.tf_callback)
+
+        # robot_state_publisher (and others?) publish on /tf_static
         self.create_subscription(tf2_msgs.msg.TFMessage, '/tf_static', self.tf_callback)
+
+        # Walk the TF tree every second
         self.create_timer(1., self.timer_callback)
         self.get_logger().info("monitor_node running")
 
     def tf_callback(self, msg: tf2_msgs.msg.TFMessage) -> None:
         for tf in msg.transforms:
-            if self.now() - rclpy.time.Time.from_msg(tf.header.stamp) > STALE_DURATION:
-                print(f'{tf.header.frame_id} => {tf.child_frame_id} is stale, ignoring')
-            else:
-                # Remove leading '/' from frame ids
-                if tf.header.frame_id[0] == '/':
-                    tf.header.frame_id = tf.header.frame_id[1:]
-                if tf.child_frame_id[0] == '/':
-                    tf.child_frame_id = tf.child_frame_id[1:]
 
-                # Find tf.header.frame_id
-                parent: Optional[Frame] = None
-                for root in self._roots:
-                    parent = root.find_frame_in_tree(tf.header.frame_id)
-                    if parent is not None:
-                        # Update the child transform
-                        parent.add_or_update_child(tf)
-                        break
+            # Remove leading '/' from frame ids
+            if tf.header.frame_id[0] == '/':
+                tf.header.frame_id = tf.header.frame_id[1:]
+            if tf.child_frame_id[0] == '/':
+                tf.child_frame_id = tf.child_frame_id[1:]
 
-                if parent is None:
-                    # This is a new child transform
-                    parent = Frame(tf.header.frame_id)
-                    parent.add_or_update_child(tf)
-                    self._roots.append(parent)
-
-        # Print the transform tree (or trees)
-        if len(self._roots) > 1:
-            print('ERROR: MULTIPLE TREES')
-        for root in self._roots:
-            root.print_tree()
-            print('===')
+            self._transforms[(tf.header.frame_id, tf.child_frame_id)] = tf
 
     def timer_callback(self) -> None:
         # Remove stale transforms
-        for root in self._roots:
-            root.delete_stale_frames_from_tree(self.now())
+        stale_keys: List[Tuple[str, str]] = []
+        for key, tf in self._transforms.items():
+            if self.now() - rclpy.time.Time.from_msg(tf.header.stamp) > rclpy.time.Duration(seconds=5):
+                print(f'!!! {key[0]} => {key[1]} is stale, deleting')
+                stale_keys.append(key)
+        for key in stale_keys:
+            self._transforms.pop(key)
 
-        # Remove empty trees
-        good: List[Frame] = []
-        for root in self._roots:
-            if root.has_children():
-                good.append(root)
-        self._roots = good
+        # Build a dict of good transforms, keyed by child_frame_id
+        frames: Dict[str, geometry_msgs.msg.TransformStamped] = {}
+
+        for tf in self._transforms.values():
+
+            # Ignore conflicting parents
+            if tf.child_frame_id in frames:
+                print(f'!!! {tf.header.frame_id} => {tf.child_frame_id} conflicts with',
+                      f'{frames[tf.child_frame_id].header.frame_id} => {tf.child_frame_id}, ignoring')
+                continue
+
+            frames[tf.child_frame_id] = tf
+
+        # Find roots
+        roots: List[geometry_msgs.msg.TransformStamped] = []
+
+        for tf in frames.values():
+            if tf.header.frame_id not in frames:
+                roots.append(tf)
+
+        # Print the tree
+        num_printed = 0
+        for tf in roots:
+            print(f'{tf.header.frame_id} => {tf.child_frame_id}: {tf_to_str(tf)}')
+            num_printed = num_printed + print_children(tf, frames) + 1
+
+        if num_printed < len(frames):
+            print(f'!!! cycle detected with {len(frames) - num_printed} transforms')
+
+        print('===')
 
 
 def main(args=None):
--- scripts/sim_node.py
+++ scripts/sim_node.py
@@ -13,13 +13,12 @@ class SimNode(rclpy.node.Node):
     def __init__(self, name: str):
         super().__init__(name)
 
+        self._sim_time = None
+
         use_sim_time = self.get_parameter('use_sim_time')
         if use_sim_time.value:
             self.get_logger().info('using sim time')
-            self._sim_time = rclpy.time.Time(clock_type=rclpy.time.ClockType.ROS_TIME)
             self.create_subscription(rosgraph_msgs.msg.Clock, '/clock', self.clock_callback)
-        else:
-            self._sim_time = None
 
     def clock_callback(self, msg: rosgraph_msgs.msg.Clock) -> None:
         self._sim_time = rclpy.time.Time.from_msg(msg.clock)
